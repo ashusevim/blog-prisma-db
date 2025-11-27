b2960e7795fa8a94f658ce83fe3d536c24af17e4
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-11-27 22:41:17 +0530

    Added /users route

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import morgan from "morgan";
+import bcrypt from "bcrypt";
+import { prisma } from "/home/wanony/projects/db-schema-and-migrations/blog-prisma-db/src/db/prisma.ts";
 
 const app = express();
 
@@ -11,6 +13,28 @@ app.get("/", (req, res) => {
 	res.status(200).json({
 		message: "API is working",
 	});
+});
+
+app.post("/users", async (req, res) => {
+	const { handle, email, password } = req.body;
+
+	if (!handle || !email || !password) {
+		res.status(400).json({
+			error: "All the fields are required",
+		});
+	}
+
+	const hashedPassword = await bcrypt.hash(password, 10);
+
+	const user = prisma.user.create({
+		data: {
+			handle,
+			email,
+			password: hashedPassword,
+		},
+	});
+
+	return res.json(user)
 });
 
 export default app;
