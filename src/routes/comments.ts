7aa17087848744d5c82c34dc268d5fc558594741
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-12-01 22:55:51 +0530

    Added comments route

---
+++ src/routes/comments.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import prisma from "../db/prisma.ts"
+
+const router = Router();
+
+router.post("", async (req, res) => {
+    const postId = req.params.
+})
--- src/routes/posts.ts
+++ src/routes/posts.ts
@@ -1,65 +1,71 @@
 import { Router } from "express";
 import { prisma } from "../db/prisma";
-import { userInfo } from "node:os";
 
 const router = Router();
 
 router.get("", async (req, res) => {
-	const page = (req.query.page as string) || 1;
-	const limit = (req.query.limit as string) || 10;
+    const page = (req.query.page as string) || 1;
+    const limit = (req.query.limit as string) || 10;
 
-	const posts = await prisma.post.findMany({
-		skip: (page - 1) * limit,
-		take: limit,
-		orderBy: { createdAt: "desc" },
-		select: {
-			id: true,
-			title: true,
-			body: true,
-			userId: true,
-			createdAt: true,
-		},
-	});
+    try {
+        const posts = await prisma.post.findMany({
+            skip: (page - 1) * limit,
+            take: limit,
+            orderBy: { createdAt: "desc" },
+            select: {
+                id: true,
+                title: true,
+                body: true,
+                userId: true,
+                createdAt: true,
+            },
+        });
 
-	res.json({
-		page,
-		limit,
-		posts,
-	});
+        res.json({
+            page,
+            limit,
+            posts,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            error: "Error fetching posts",
+        });
+    }
 });
 
 router.post("", async (req, res) => {
-	const { title, body, userId } = req.body;
+    const { title, body, userId } = req.body;
 
-	if (!title || !userId) {
-		return res.status(400).json({
-			message: "All the fields are required",
-		});
-	}
+    if (!title || !userId) {
+        return res.status(400).json({
+            message: "All the fields are required",
+        });
+    }
 
-	try {
-		const post = await prisma.user.create({
-			data: {
-				title,
-				body,
-				userId, // prisma enforces foreign key to user.id
-			},
-			select: {
-				id: true,
-				title: true,
-				body: true,
-				userId: true,
-				createdAt: true,
-			},
-		});
+    try {
+        const post = await prisma.user.create({
+            data: {
+                title,
+                body,
+                userId, // prisma enforces foreign key to user.id
+            },
+            select: {
+                id: true,
+                title: true,
+                body: true,
+                userId: true,
+                createdAt: true,
+            },
+        });
 
-		res.json(post);
-	} catch (err) {
-		console.log(err);
-		res.status(400).json({
-			error: "could not create post",
-		});
-	}
+        res.json(post);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({
+            error: "could not create post",
+        });
+    }
 });
 
 export default router;
