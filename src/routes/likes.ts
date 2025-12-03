8affc63788c938e6741f5725d559a3871a64f83f
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-12-03 12:56:09 +0530

    fixed likes route

--- src/routes/likes.ts
+++ src/routes/likes.ts
@@ -4,25 +4,32 @@ import { prisma } from "../db/prisma";
 const router = Router();
 
 router.post("/posts/:id/likes", async (req, res) => {
-	const postId = parseInt(req.params.id);
-	const { userId } = req.body;
+    const postId = parseInt(req.params.id);
+    const { userId } = req.body;
 
-	try {
-		const like = await prisma.like.create({
-			data: {
-				postId,
-				userId,
-			},
-			select: {
-				id: true,
-				postId: true,
-				userId: true,
-				createdAt: true,
-			},
-		});
-	} catch (error) {
-		console.log(error);
-	}
+    if (!userId) {
+        console.log("UserId is required");
+    }
+
+    try {
+        const like = await prisma.like.create({
+            data: {
+                postId,
+                userId,
+            },
+            select: {
+                id: true,
+                postId: true,
+                userId: true,
+                createdAt: true,
+            },
+        });
+
+        res.json(like);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Failed to like on post" });
+    }
 });
 
-export default router
+export default router;
