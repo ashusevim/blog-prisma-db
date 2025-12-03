ab14acf61662270beffe71dfa650fd551206b24c
Ashish Prajapati <108178021+codingashishdev@users.noreply.github.com>
2025-12-03 22:21:42 +0530

    refactor: update app structure and improve server initialization
    - Changed the entry point for the development script from src/index.ts to src/app.ts.
    - Updated the app initialization to use express instead of Router.
    - Added express.json() middleware for JSON request parsing.
    - Corrected route definitions for comments and likes.
    - Added conditional server listening based on NODE_ENV.
    - Removed the old index.ts file as it is no longer needed.

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,34 @@
-import { Router } from "express";
+import express from "express";
 import cors from "cors";
 import morgan from "morgan";
 import usersRouter from "./routes/users";
-import postsRouter from "./routes/posts"
-import commentsRouter from "./routes/comments"
-import likesRouter from "./routes/likes"
+import postsRouter from "./routes/posts";
+import commentsRouter from "./routes/comments";
+import likesRouter from "./routes/likes";
 
-const app = Router();
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(morgan("dev"));
+app.use(express.json())
 
-app.use("/users", usersRouter)
-app.use("/posts", postsRouter)
-app.use("", commentsRouter)
-app.use("", likesRouter)
+app.use("/users", usersRouter);
+app.use("/posts", postsRouter);
+app.use("/comments", commentsRouter);
+app.use("/likes", likesRouter);
 
 app.get("/", (req, res) => {
-	res.status(200).json({
-		message: "API is working",
-	});
+    res.status(200).json({
+        message: "API is working",
+    });
 });
+
+if(process.env.NODE_ENV !== "test"){
+    app.listen(PORT, () => {
+        console.log(`Listening to the port: `, PORT);
+    });
+}
+
 
 export default app;
